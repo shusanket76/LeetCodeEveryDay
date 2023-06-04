62211464f77a3ff9ee4cc220188a9e7ff23d29d2
shusanket <shusanketbasyal76@gmail.com>
2023-06-04 11:22:57 -0400

    Day06

---
+++ Day06/BestTimeToBuyAndSellStock.py
@@ -0,0 +1,19 @@
+# link="https://leetcode.com/problems/best-time-to-buy-and-sell-stock/"
+def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        l=0
+        r=l+1
+        profit = 0
+        while r<=len(prices)-1:
+            if prices[l]>prices[r]:
+                l=l+1
+                r=l+1
+            else:
+                profit = max(profit, prices[r]-prices[l])
+                r=r+1
+        return profit
+            
+            
---
+++ Day06/ContainerWithMostWater.py
@@ -0,0 +1,21 @@
+# link = https://leetcode.com/problems/container-with-most-water/
+
+def maxArea(height):
+    
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        res=0
+        l,r=0,len(height)-1
+        while l<r:
+            h = min(height[l],height[r])
+            b=r-l
+            res = max(res,h*b)
+            if height[l]<height[r]:
+                l=l+1
+            else:
+                r=r-1
+        return res 
+a = maxArea([1,8,6,2,5,4,8,3,7])
+print(a)
