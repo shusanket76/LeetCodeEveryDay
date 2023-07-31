1845b9a5812b33e6f1b981f4de349c31f9f44b84
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-07-30 22:00:50 -0400

    Day44

--- p.py
+++ p.py
@@ -1,16 +1,9 @@
-def characterReplacement(s: str, k: int):
-        hasmap = {}
-        l=r=0
-        res= 0
-        while r<len(s):
-            hasmap[s[r]]=hasmap.get(s[r], 0)+1
-            while (r-l+1)-max(hasmap.values())>k:
-                hasmap[s[l]] = hasmap.get(s[l],0)-1
-                l+=1
-            else:
-                res = max(res, r-l+1)
-                r+=1
-        return res
-        
-a = characterReplacement("AABABBA", 1)   
-print(a)
+from collections import deque
+
+shusa = deque()
+
+shusa.appendleft(12)
+shusa.append(21)
+
+print(shusa)
+print(shusa[0])
