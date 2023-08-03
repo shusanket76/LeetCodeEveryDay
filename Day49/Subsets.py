92a9a23c4206b9b3c7e50ea69854631211fbb6b0
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-08-03 18:01:20 -0400

    Day49

---
+++ Day49/Subsets.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subsets(self, nums: [int]) ->[[int]]:
+
+
+        res = [[]]
+        def dfs(nums, pointer,curr):
+            if  pointer>len(nums)-1:
+                return curr
+            curr.append(nums[pointer])
+            newcurr = curr[:]
+            res.append(newcurr)
+            dfs(nums, pointer+1, curr)
+            curr.pop()
+            dfs(nums,pointer+1, curr)
+            
+
+            
+            
+        pointer = 0
+        dfs(nums, pointer, [])
+        return res
