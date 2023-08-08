d0016707328a22be9b6ce212c7e862d7ccc169b7
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-08-08 13:27:05 -0400

    Day49

--- Day49/Subsets.py
+++ Day49/Subsets.py
@@ -2,6 +2,22 @@ class Solution:
     def subsets(self, nums: [int]) ->[[int]]:
         res = [[]]
         def dfs(nums, pointer,curr):
+
+            # =======================================================
+
+            #     if pointer>len(nums)-1:
+            #     if curr not in res:
+            #         res.append(curr[:])
+            #     return 
+
+    
+            
+            # curr.append(nums[pointer])
+            
+            # dfs(curr,pointer+1)
+            # curr.pop()
+            # dfs(curr,pointer+1)
+            # ===============================================
             if  pointer>len(nums)-1:
                 return curr
             curr.append(nums[pointer])
