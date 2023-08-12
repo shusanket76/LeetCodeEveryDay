669941f3f06101b4e24a51e23586e050bf8d5d33
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-08-11 21:38:53 -0400

    Day5Up

--- Day5/ValidPalindrome.py
+++ Day5/ValidPalindrome.py
@@ -7,4 +7,21 @@ def isPalindrome(s):
             newS+=x.lower()
 
     return(newS==newS[::-1])
+# # ====================SOL-2==================================+++?
+#     class Solution:
+#     def isPalindrome(self, s: str) -> bool:
+#         l=0
+#         r=len(s)-1
+#         alpha = "abcdefghijklmnopqrstuvwxyz0123456789"
+#         while l<r:
+#             while l<r and  s[l].lower() not in alpha:
+#                 l+=1
+#             while l<r and s[r].lower() not in alpha:
+#                 r-=1
+#             if s[l].lower()!=s[r].lower():
+#                 return False
+#             l+=1
+#             r-=1
+#         return True
+
 a = isPalindrome("A man, a plan, a canal: Panama")
--- p.py
+++ p.py
@@ -1,5 +1,1 @@
-for a in range(10):
-    for b in range(5):
-        if b==5:
-            break
-        print(b)
+print(abs(5-9))
