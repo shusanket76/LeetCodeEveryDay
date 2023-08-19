817a08d0c48adb446719153b31980b89181774e2
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-08-19 11:55:55 -0400

    Day55

---
+++ Day55/TrappingRainWater.py
@@ -0,0 +1,20 @@
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        maxleft = [0 for x in range(len(height))]
+        maxright = [0 for x in range(len(height))]
+        mal = 0
+        for x in range(len(height)):
+            mal = max(mal, height[x])
+            maxleft[x] = mal
+        mar = 0 
+        for y in range(len(height)-1,-1,-1):
+            mar = max(mar, height[y])
+            maxright[y] = mar
+
+        res = 0
+        for z in range(len(height)):
+            a = min(maxleft[z], maxright[z])
+            b = a-height[z]
+            if b>0:
+                res+=b
+        return res
