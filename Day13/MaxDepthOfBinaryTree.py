463b3ac15ec0f23f013c7a4f0cff374ad6a1f240
SHUSANKET BASYAL <85852621+shusanket76@users.noreply.github.com>
2023-08-24 09:22:12 -0400

    Day13

--- Day13/MaxDepthOfBinaryTree.py
+++ Day13/MaxDepthOfBinaryTree.py
@@ -16,4 +16,22 @@ class Solution(object):
         a=self.maxDepth(root.left)+1
         b = self.maxDepth(root.right)+1
         return max(a,b)
+# ========================================================================================++++?\
+# SOLUTION2
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root) -> int:
+        res = [0]
+        def dfs(root, count):
+            if root:
+                res[0] = max(res[0], count)
+                dfs(root.left, count+1)
+                dfs(root.right, count+1)
+        dfs(root, 1)
+        return res[0]
         
